6c68679965c388adbae94f279da1e7b2b8eb0bb6
TheOr_da <135665465+arandomguyhi@users.noreply.github.com>
2025-02-02 09:45:05 -0300

    printing

--- scripts/camshit.lua
+++ scripts/camshit.lua
@@ -3,8 +3,13 @@ setVar('cameraPoint', {x = nil, y = nil})
 function onUpdate()
     for i = 1,2 do
         if getVar('cameraPoint')[i] ~= nil then
+            setProperty('isCameraOnForcedPos', true)
             setProperty('camFollow.x', getVar('cameraPoint').x)
             setProperty('camFollow.y', getVar('cameraPoint').y)
+            debugPrint('testingggg')
+        else
+            debugPrint('tafalso')
+            setProperty('isCameraOnForcedPos', false)
         end
     end
 end
