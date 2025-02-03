927e6b1ec71909c331daa234c0a06b2d98c4aa72
TheOr_da <135665465+arandomguyhi@users.noreply.github.com>
2025-02-03 10:26:16 -0300

    added camOverlay

--- scripts/camshit.lua
+++ scripts/camshit.lua
@@ -1,6 +1,20 @@
 local points = {x = nil, y = nil}
 setVar('cameraPoint', points)
 
+function onCreate()
+    runHaxeCode([[
+        var camOverlay = new FlxCamera();
+        camOverlay.bgColor = 0x0;
+        setVar('camOverlay', camOverlay);
+
+        FlxG.cameras.remove(game.camHUD, false);
+        FlxG.cameras.remove(game.camOther, false);
+
+        for (cams in [camOverlay, game.camHUD, game.camOther])
+            FlxG.cameras.add(cams, false);
+    ]])
+end
+
 function onUpdate()
     if getVar('cameraPoint').x ~= nil and getVar('cameraPoint').y ~= nil then
         setProperty('isCameraOnForcedPos', true)
