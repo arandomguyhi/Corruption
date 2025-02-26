f30562f577e2ad4aefb43bdd04bcf92524f3f76d
TheOr_da <135665465+arandomguyhi@users.noreply.github.com>
2025-02-26 16:24:28 -0300

    fixed saving

--- scripts/saves.lua
+++ scripts/saves.lua
@@ -5,6 +5,12 @@ setDataFromSave('corruptMenu', 'main', getDataFromSave('corruptMenu', 'main'))
 setDataFromSave('corruptMenu', 'if', getDataFromSave('corruptMenu', 'if'))
 setDataFromSave('corruptMenu', 'credits', getDataFromSave('corruptMenu', 'credits'))
 
+for _, i in pairs({'main', 'credits', 'if'}) do
+    if getDataFromSave('corruptMenu', i) == nil then
+        setDataFromSave('corruptMenu', i, i == 'main' and true or false)
+    end
+end
+
 -- notice shit
 setDataFromSave('corruptMenu', 'seenIfNotice', getDataFromSave('corruptMenu', 'seenIfNotice'))
 setDataFromSave('corruptMenu', 'seenSidestoryNotice', getDataFromSave('corruptMenu', 'seenSidestoryNotice'))
